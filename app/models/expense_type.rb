0a3cfe75ecc6d5680944db5839a94ed8d6949615
wdong159 <david2015wd159@gmail.com>
2019-04-02 23:32:28 +0000

    receipts can be delete in a form

--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -1,6 +1,20 @@
 class ExpenseType < ApplicationRecord
+    before_destroy :ensure_not_referenced_by_any_wishes_receipts
     has_many :wishes
     has_many :receipts
     
     validates :name, presence: true
+    
+    private
+    def ensure_not_referenced_by_any_wishes_receipts
+        unless wishes.empty?
+            errors.add(:base, 'There are wishes associated with this type')
+            throw :abort
+        end
+        
+        unless receipts.empty?
+            errors.add(:base, 'There are receipts associated with this type')
+            throw :abort
+        end
+    end
 end
