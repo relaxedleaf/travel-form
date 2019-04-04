c6b432ba788b4166279ee8168222cf0d4c3508e5
Guanghui Li <liguanghui.cs@gmail.com>
2019-04-04 06:00:06 +0000

    fixed an error in the migration file, need to reset the db

--- db/migrate/20190404032530_change_department_type_in_request.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDepartmentTypeInRequest < ActiveRecord::Migration[5.1]
-  def change
-    change_column :requests, :department_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190404032530) do
+ActiveRecord::Schema.define(version: 20190403184137) do
 
   create_table "authorization_forms", force: :cascade do |t|
     t.integer "status_id"
