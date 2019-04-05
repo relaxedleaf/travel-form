7389d6e048c7f84dd36d46c5bd8929935b095c14
Guanghui Li <liguanghui.cs@gmail.com>
2019-04-05 18:19:35 +0000

    added date validation in trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,7 +5,26 @@ class Trip < ApplicationRecord
     has_many :requests, :dependent => :destroy
 
     validates :purpose, :destination, :date_start, :date_end, :employee_id, presence: true
+    validate :dates_validation
+    
     
     accepts_nested_attributes_for :authorization_form
     accepts_nested_attributes_for :requests
+    
+    private
+    
+    def dates_validation
+        ds = date_start.split('/').map(&:to_i) #date start
+        de = date_end.split('/').map(&:to_i) #date end
+        if de[2] < ds[2]
+            errors.add(:date_end,"can't be ealier than date start")
+            throw :abort
+        elsif de[0] < ds[0]
+            errors.add(:date_end,"can't be ealier than date start")
+            throw :abort
+        elsif de[1] < ds[1]
+            errors.add(:date_end,"can't be ealier than date start")
+            throw :abort
+        end
+    end
 end
