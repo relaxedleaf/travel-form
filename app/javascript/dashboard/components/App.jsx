2ea2a0330a16e68b60ded61a6046b7fc3da56c01
Marko <marko_XLR8@hotmail.com>
2019-04-16 16:52:06 -0400

    Finished forms pie chart, started on dept bar graph.

--- app/javascript/dashboard/components/App.jsx
+++ app/javascript/dashboard/components/App.jsx
@@ -1,47 +1,14 @@
 import React from 'react';
-import ReactChartkick, { LineChart, PieChart } from 'react-chartkick';
-import Chart from 'chart.js';
-import axios from 'axios';
-
-ReactChartkick.addAdapter(Chart)
+//import ReactChartkick, { LineChart, PieChart } from 'react-chartkick';
+//import Chart from 'chart.js';
+//import axios from 'axios';
+//import piechart from 'Piechart'
     
-export default class App extends React.Component {    
+export default class App extends React.Component {
+/* render (<Piechart />
+           <Graph />)*/    
   state = {
     statuses: [],
     num: []
-  };
-  
-  componentDidMount = () => {
-    var self = this;
-    axios.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest";
-    axios.get('/api/allstatus')
-      .then(function (response) {
-          self.setState({statuses: response.data.statuses})
-          self.setState({num: response.data.num})
-          
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-  render = () => {
-    if(this.state.statuses.length != 0 && this.state.num.length !=0){
-        console.log(this.state.statuses);
-        return(
-
-          <PieChart data={
-                           [[this.state.statuses[0], this.state.num[0]],
-                            [this.state.statuses[1], this.state.num[1]],
-                            [this.state.statuses[2], this.state.num[2]],
-                            [this.state.statuses[3], this.state.num[3]],
-                            [this.state.statuses[4], this.state.num[4]]]
-                          } />
-        )}
-
-    else{
-        return <div>Loading...</div>
-    }      
-  
   };
 }
---
+++ app/javascript/dashboard/components/DeptGraph.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactChartkick, { ColumnChart } from 'react-chartkick';
+import Chart from 'chart.js';
+import axios from 'axios';
+
+ReactChartkick.addAdapter(Chart)
+
+export default class App extends React.Component {
+
+    componentDidMount = () => {
+        var self = this;
+        axios.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest";
+        axios.get('/api/allstatus')
+        .then(function (response) {
+            self.setState({statuses: response.data.statuses})
+            self.setState({num: response.data.num})
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    };
+
+    render = () => {
+        if(this.state.statuses.length != 0 && this.state.num.length !=0){
+            console.log(this.state.statuses);
+            return(
+
+            <ColumnChart data={
+                            [[this.state.statuses[0], this.state.num[0]],
+                                [this.state.statuses[1], this.state.num[1]],
+                                [this.state.statuses[2], this.state.num[2]],
+                                [this.state.statuses[3], this.state.num[3]],
+                                [this.state.statuses[4], this.state.num[4]]]
+                            } />
+            )}
+
+        else{
+            return <div>Loading...</div>
+        }      
+    
+    };
+}
