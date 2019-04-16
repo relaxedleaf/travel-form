be7efdc7e8ee52baf9d7abeaf5c4c1dcf1c415f7
Marko <marko_XLR8@hotmail.com>
2019-04-16 12:41:15 -0400

    Started implementing React into dashboard

--- app/javascript/authorization_form/index.js
+++ app/javascript/authorization_form/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App"
 
 document.addEventListener("DOMContentLoaded", () => {
-    const authorization_form = document.querySelector("#authorization_form");
-    ReactDOM.render(<App />, authorization_form);
+    const dashboard = document.querySelector("#dashboard");
+    ReactDOM.render(<App />, dashboard);
 });
